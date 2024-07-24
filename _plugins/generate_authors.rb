2c00cde13e987d4269b5c22c0c1c7d0150b04e96
colloquia-latina <colloquialatina@proton.me>
2024-07-24 16:55:22 -0500

    added author pages stuff

---
+++ _plugins/generate_authors.rb
@@ -0,0 +1,28 @@
+# _plugins/generate_authors.rb
+module Jekyll
+    class AuthorPageGenerator < Generator
+      safe true
+  
+      def generate(site)
+        authors = site.collections["colloquia"].docs.map { |doc| doc.data["author"] }.uniq
+        authors.each do |author|
+          site.pages << AuthorPage.new(site, site.source, "author", author)
+        end
+      end
+    end
+  
+    class AuthorPage < Page
+      def initialize(site, base, dir, author)
+        @site = site
+        @base = base
+        @dir = dir
+        @name = "#{author.slugify}.html"
+  
+        self.process(@name)
+        self.read_yaml(File.join(base, '_layouts'), 'author.html')
+        self.data['author'] = author
+        self.data['title'] = "#{author}'s Colloquia"
+      end
+    end
+  end
+  
